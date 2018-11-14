0cb818db626de52648ca76b2e0f91863aa3f87fb
João Abecasis <joao@schibsted.com>
2018-11-14 14:52:48 +0100

    Fix console script

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     entry_points={
         'console_scripts': [
-            'jref=jref', 
+            'jref=jref.__main__:main',
         ]
     },
 )
