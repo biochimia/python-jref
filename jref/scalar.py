655cc1cb06758368e23ab893f4ba976c8abd8674
João Abecasis <joao@abecasis.name>
2021-05-28 15:14:24 +0200

    Make flake8 happy?

--- jref/scalar.py
+++ jref/scalar.py
@@ -4,6 +4,13 @@ import sys
 
 
 if sys.version_info.major == 3:
+    # For the sake of static checkers:
+    # - flake8: F821 undefined name 'unicode'
+    # - flake8: F821 undefined name 'long'
+    unicode = str
+    long = int
+
+if sys.version_info.major == 3:
     _STRING_TYPES = (bytes, str,)
     _NUMERIC_TYPES = (int, float)
 else:
